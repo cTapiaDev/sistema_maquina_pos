7e41d202eb1bf758048174dc3b82a26d26025476
ctapiadev <ctapiadev@gmail.com>
2024-07-12 10:54:57 -0400

    sistema funcionando

--- src/sistemapos/Persona.java
+++ src/sistemapos/Persona.java
@@ -24,6 +24,50 @@ public class Persona {
         this.email = email;
     }
 
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String nombreCompleto() {
+        return getNombre() + " " + getApellido();
+    }
+
     @Override
     public String toString() {
         return "Persona{" 
--- src/sistemapos/PointOfSale.java
+++ src/sistemapos/PointOfSale.java
@@ -6,6 +6,34 @@ public class PointOfSale {
     public static final int MIN_COUNT_CUOTAS = 1;
     public static final int MAX_COUNT_CUOTAS = 6;
     
-    public PointOfSale() {};
+    public Ticket realizarPago(
+            TarjetaDeCredito tarjeta,
+            double montoAPagar,
+            int cantidadCuotas
+    ) {
+        // Monto Final a pagar, considerando el recargo por la cantidad de cuotas
+        double montoFinal = montoAPagar + (montoAPagar * recargoPorCuotas(cantidadCuotas));
+        
+        Ticket nuevoTicket = null;
+        
+        if (tarjeta.tieneSaldoDisponible(montoFinal)) {
+            double montoPorCuota = montoFinal / cantidadCuotas;
+            tarjeta.descontar(montoPorCuota);
+            String cliente = tarjeta.nombreTitular();
+            
+            nuevoTicket = new Ticket(
+                    cliente, 
+                    montoFinal, 
+                    montoPorCuota
+            );  
+        }
+        
+        return nuevoTicket;
+        
+    }
+    
+    private double recargoPorCuotas(int cantidadCuotas) {
+        return (cantidadCuotas - 1) * RECARGO_POR_CUOTA;
+    }
     
 }
